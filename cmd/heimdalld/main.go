434ff20da3973ba4eccc133002d6326b6c25eb1d
clarkchenc <651044554@qq.com>
2023-03-22 15:59:15 +0800

    fix lint

--- cmd/heimdalld/main.go
+++ cmd/heimdalld/main.go
@@ -175,7 +175,8 @@ func getNewApp(serverCtx *server.Context) func(logger log.Logger, db dbm.DB, sto
 		helper.InitDeliveryConfig("")
 		helper.UpdateTendermintConfig(serverCtx.Config, viper.GetViper())
 		// create new heimdall app
-		return app.NewHeimdallApp(logger, db, baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))))
+		return app.NewHeimdallApp(logger, db,
+			baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))))
 	}
 }
 
