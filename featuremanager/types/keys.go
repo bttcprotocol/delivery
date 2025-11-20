af94440b74d0309f25cf3a89ad8918eb274716ca
codymeng <mengxiaole269@gmail.com>
2025-11-20 15:06:55 +0800

    feat(featuremanager): add TronDynamicCheckpoint key

--- featuremanager/types/keys.go
+++ featuremanager/types/keys.go
@@ -18,9 +18,9 @@ const (
 )
 
 const (
-	SupportMapMarshaling = "SupportMapMarshaling"
-	DynamicCheckpoint    = "DynamicCheckpoint"
-	FinalizedEth         = "FinalizedEth"
-	NoAckValidatorCheck  = "NoAckValidatorCheck"
+	SupportMapMarshaling  = "SupportMapMarshaling"
+	DynamicCheckpoint     = "DynamicCheckpoint"
+	FinalizedEth          = "FinalizedEth"
+	NoAckValidatorCheck   = "NoAckValidatorCheck"
 	TronDynamicCheckpoint = "TronDynamicCheckpoint"
 )
